f84160cb48d954333f6e880d372d16eedf732e42
Bjrzum <86805668+Bjrzum@users.noreply.github.com>
2022-01-21 11:06:02 -0500

    Add files via upload

---
+++ es/traducir.php
@@ -0,0 +1,8 @@
+<?php
+$texto_idre__nav__valueInputSubmit = "Bosnn Esspannl";
+$texto_espanol__nav__valueInputSubmit = "Idioma IDRE";
+$texto_idre__nav__a = "Leveral xa Mittomles";
+$texto_espanol__nav__a = "Contactar al Programador";
+$texto_espanol_barco = "Navega por la Imaginación.";
+$texto_idre_barco = "Bome rënlu sheamte laftam Cò";
+?>
